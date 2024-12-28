9e5150f38f9c8e81e9efb05f99650a215086597a
Mauro Campos <mauro.pcsergio@gmail.com>
2024-12-28 10:38:03 -0300

    FEAT/SAVE

--- main.tf
+++ main.tf
@@ -4,16 +4,6 @@ module "palworld" {
   instance_type     = "t3.xlarge"
   create_ssh_key    = true
   ssh_public_key    = "palworld-ssh.pub"
-  start_from_backup = true
-
-  # backup_files_storage_type       = "local"
-  # backup_files_local_path         = "./assets/61862B4A464ECD23A94B96BE1374EF3A"
-  # create_backup_s3_bucket         = true
-  # s3_bucket_backup_retention      = 7
-  # backup_s3_bucket_arn            = ""
-  # backup_s3_bucket_name           = ""
-  # backup_interval_cron_expression = "*/5 * * * *"
-  # dedicated_server_name_hash      = "61862B4A464ECD23A94B96BE1374EF3A"
 
   use_custom_palworldsettings = true
   custom_palworldsettings_s3  = true
