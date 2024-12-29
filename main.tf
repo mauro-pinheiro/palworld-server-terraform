a6ff6f053348c02beb66f03ca5376b0d53eb7afc
Mauro Campos <mauro.pcsergio@gmail.com>
2024-12-29 06:41:36 -0300

    fix: force destroy false

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ module "palworld" {
   create_ssh_key    = true
   ssh_public_key    = "palworld-ssh.pub"
   start_from_backup = true
+  force_destroy     = false
 
   backup_files_storage_type       = "local"
   backup_files_local_path         = "./assets/61862B4A464ECD23A94B96BE1374EF3A"
