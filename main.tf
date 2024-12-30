afa6a75f62df65575f672f3bbfc471e64b77cbbe
Mauro Campos <mauro.pcsergio@gmail.com>
2024-12-29 21:13:27 -0300

    backups dont work

--- main.tf
+++ main.tf
@@ -9,11 +9,6 @@ module "palworld" {
   force_destroy                   = false
   backup_files_storage_type       = "local"
   backup_files_local_path         = "./assets/61862B4A464ECD23A94B96BE1374EF3A"
-  create_backup_s3_bucket         = true
-  s3_bucket_backup_retention      = 7
-  backup_s3_bucket_arn            = ""
-  backup_s3_bucket_name           = ""
-  backup_interval_cron_expression = "*/5 * * * *"
   dedicated_server_name_hash      = "61862B4A464ECD23A94B96BE1374EF3A"
 
   use_custom_palworldsettings = true
